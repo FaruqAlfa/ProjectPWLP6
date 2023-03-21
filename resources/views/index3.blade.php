c810c7fa0f13dc91f7a1db694400f4819140f185
FaruqAlfa <alfafahmi172@gmail.com>
2023-03-21 16:22:34 +0700

    Praktikum 4

--- resources/views/index3.blade.php
+++ resources/views/index3.blade.php
@@ -40,16 +40,6 @@
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
       </li>
-        <liclass="nav-item d-none d-sm-inline-block">
-          <a href="nav-link" href="{{ route('logout') }}"
-        onclick="event.preventDefault();
-                      document.getElementById('logout-form').submit();">
-                      {{ _('Logout') }}
-        </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-        @csrf
-      </form>
-        </liclass=>
 
     </ul>
 
@@ -173,7 +163,16 @@
           <i class="fas fa-th-large"></i>
         </a>
       </li>
-      
+      <li class="nav-item d-none d-sm-inline-block">
+        <a class="nav-link" href="{{ route('logout')}}"
+        onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+            {{__('Logout') }}
+        </a>
+        <form id="logout-form" action="{{route('logout') }}" method="POST" class="d-none">
+          @csrf
+        </form>
+      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
